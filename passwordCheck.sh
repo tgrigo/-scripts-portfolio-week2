b2608ccc5ddbabac2a8b2a2c5dcec84b90127589
Todd G <65108263+tgrigo@users.noreply.github.com>
2020-06-21 20:10:26 +0800

    Add files via upload

--- passwordCheck.sh
+++ passwordCheck.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
-echo
+# Clear the terminal screen
+clear
+
 # The -e \c  command lets the user input onto the same line as the text question
-echo -e "Please Enter your User Password (hidden): \c"
+# The default password is 1234 based on the secret.txt file
+echo
+echo -e "Please Enter Your User Password (hidden): \c"
+
 # The -s hides the types input
 read -s passwordCheck
 
 #The $passwordCheck variable is converted to sha256sum and then checked against the specified file contents
+echo
 echo "$passwordCheck" | sha256sum --check --status ./secret.txt
 
 # The if command compares the last output to the true (0) or false (1) outcome
 if [[ $? -eq 0 ]];
 
 then
-echo
-echo "Your access is granted."
-echo
-exit 0
+    echo
+    echo -e "\033[1;33mYour access is granted.\033[0m"
+    echo
+    exit 0
 
 else
-echo
-echo "We are sorry your access is denied as your passwords is incorrect."
-echo
-exit 1
+    echo
+    echo -e "\033[1;31mWe are sorry your access is denied as your passwords is incorrect.\033[0m"
+    echo
+    exit 1
 
 fi
+
+exit 0
 
